c849ac87d4194932c87baea3732cd5d9a3f38d29
Robert Lewis <caysonlewis@gmail.com>
2026-02-07 20:02:38 -0600

    finalized PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,22 +7,25 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    public float speed = 0;
 
     void Start()
     {
         rb = GetComponent <Rigidbody>();
     }
 
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-        rb.AddForce(movement);
-    }
-
-    void onMove (InputValue movementValue)
+    void OnMove (InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+
 }
